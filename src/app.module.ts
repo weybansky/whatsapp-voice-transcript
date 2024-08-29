f33590197b8a4af595dded3a2aacc101b6b51a4f
Abdulwahab Nasir <weybansky@gmail.com>
2024-08-29 06:32:12 +0100

    config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OpenaiService } from './openai/openai.service';
+import { WhatsappService } from './whatsapp/whatsapp.service';
+import { WhatsappController } from './whatsapp/whatsapp.controller';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot()],
+  controllers: [AppController, WhatsappController],
+  providers: [AppService, OpenaiService, WhatsappService],
 })
 export class AppModule {}
